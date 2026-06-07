c3eb8dbd4fe3cc2f05b711ea3b0ecd0be8a09610
jpirra <pirrajuanpablo@gmail.com>
2026-06-07 18:11:05 +0000

    fix: eliminar ObjectMapper custom que bloqueaba JavaTimeModule de Spring Boot

--- backend/src/main/java/com/jafpsoft/ventas/config/AppConfig.java
+++ backend/src/main/java/com/jafpsoft/ventas/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.jafpsoft.ventas.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,10 +12,5 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
     }
 }
